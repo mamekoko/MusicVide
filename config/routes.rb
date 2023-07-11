f55e4c87923f390bf0788e8823ef26bf6416dab9
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-07-11 10:30:15 +0000

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  namespace :public do
-    get 'homes/top'
+  devise_for :users
+  
+  scope module: 'public' do
+    root "homes#top"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
