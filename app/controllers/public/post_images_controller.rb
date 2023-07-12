eb5dc19747d1ae4f0633d78c29c5b3988a69bfec
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-07-12 11:08:00 +0000

    up

---
+++ app/controllers/public/post_images_controller.rb
@@ -0,0 +1,11 @@
+class Public::PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   
   scope module: 'public' do
     root "homes#top"
+    
+    resources :post_images, only: [:new, :index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
