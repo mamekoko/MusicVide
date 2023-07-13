0128fb6d83ef90d6e877fdb84ae3cc47c9698d58
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-07-13 10:48:15 +0000

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  
+
   scope module: 'public' do
     root "homes#top"
-    
-    resources :post_images, only: [:new, :index, :show]
+
+    resources :post_images, only: [:new, :create, :index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
