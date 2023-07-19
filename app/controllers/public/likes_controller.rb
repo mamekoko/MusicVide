d02a6a4e7fb601ed4a817b18020be9f4efcc0595
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-07-19 16:37:28 +0000

    Up

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,20 +1,24 @@
 class Public::LikesController < ApplicationController
 
   before_action :authenticate_user!, only: [:create, :destroy]
-  before_action :post_image_params, only: [:create, :destroy]
+  before_action :set_post_image, only: [:create, :destroy]
 
   def create
-    Like.create(user_id: current_user.id, post_image_id: @post_image.id)
+    @post_image.likes.create(user_id: current_user.id)
+
+
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, post_image_id: @post_image.id)
-    like.destroy
+    like&.destroy
+
+
   end
 
   private
 
-  def post_imaage_params
+  def set_post_image
     @post_image = PostImage.find(params[:post_image_id])
   end
 
